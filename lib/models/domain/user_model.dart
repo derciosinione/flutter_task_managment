62359c69a0b9b7a04be45af85e9aa534762b4422
derciosinione <derciosinione@gmail.com>
2024-06-19 16:29:16 +0100

    Created login feature

---
+++ lib/models/domain/user_model.dart
@@ -0,0 +1,27 @@
+class User {
+  final String name;
+  final String email;
+  final String phoneNumber;
+
+  User({
+    required this.name,
+    required this.email,
+    required this.phoneNumber,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      name: json['name'] ?? '',
+      email: json['email'] ?? '',
+      phoneNumber: json['phoneNumber'] ?? '',
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'email': email,
+      'phoneNumber': phoneNumber,
+    };
+  }
+}
